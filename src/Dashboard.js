03688e873b8e43dd97a6b9575e57055bae3ecc20
ali3amer <aliaamer549@gmail.com>
2022-02-14 03:08:04 +0200

    13-02-2022 office v2

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,6 +3,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { auth, db, logout } from "./firebase";
 import { query, collection, getDocs, where } from "firebase/firestore";
+import Navbar from "./components/Navbar";
 
 function Dashboard() {
   const [user, loading, error] = useAuthState(auth);
@@ -13,8 +14,8 @@ function Dashboard() {
     try {
       const q = query(collection(db, "users"), where("uid", "==", user?.uid));
       const doc = await getDocs(q);
+      console.log(doc);
       const data = doc.docs[0].data();
-
       setName(data.name);
     } catch (err) {
       console.error(err);
@@ -31,14 +32,15 @@ function Dashboard() {
 
   return (
     <div className="dashboard">
-      <div className="dashboard__container">
+      <Navbar />
+      {/* <div className="dashboard__container">
         Logged in as
         <div>{name}</div>
         <div>{user?.email}</div>
         <button className="dashboard__btn" onClick={logout}>
           Logout
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
